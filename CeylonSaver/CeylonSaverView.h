3869f18b7d41aa0dbb21a0643b4b25c66223d088
Lahiru Himesh Madusanka <lahiru.lahirumadusanka@gmail.com>
2022-09-03 13:29:15 +0530

    Initial Commit

---
+++ CeylonSaver/CeylonSaverView.h
@@ -0,0 +1,12 @@
+//
+//  CeylonSaverView.h
+//  CeylonSaver
+//
+//  Created by Lahiru Himesh Madusanka on 2022-09-03.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface CeylonSaverView : ScreenSaverView
+
+@end
